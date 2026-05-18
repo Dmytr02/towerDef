f250a819f8011de3b43b7db204f954efffa9a7b5
mmysak <mmysak@edu.cdv.pl>
2026-05-18 21:53:05 +0200

    audios manager

---
+++ Assets/Scripts/Audio/SoundEffectLibrary.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectLibrary : MonoBehaviour {
+	[SerializeField] private SoundEffectGroup[] soundEffectGroups;
+	Dictionary<string, List<AudioClip>> soundDictionary;
+
+	void InitializeDictionary() {
+		if (soundDictionary != null) return;
+
+		soundDictionary = new Dictionary<string, List<AudioClip>>();
+		foreach (SoundEffectGroup group in soundEffectGroups) {
+			if (string.IsNullOrEmpty(group.name)) continue;
+			soundDictionary[group.name] = group.audioClips;
+		}
+	}
+
+	public AudioClip GetRandomClip(string soundName) {
+		InitializeDictionary();
+
+		if (soundDictionary.TryGetValue(soundName, out List<AudioClip> clips)) {
+			if (clips != null && clips.Count > 0) {
+				return clips[Random.Range(0, clips.Count)];
+			}
+		}
+		Debug.LogWarning($"SoundEffectLibrary: No one for '{soundName}'");
+
+		return null;
+	}
+}
+
+[System.Serializable]
+public struct SoundEffectGroup {
+	public string name;
+	public List<AudioClip> audioClips;
+}
---
+++ Assets/Scripts/Audio/SoundEffectManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SoundEffectLibrary))]
+public class SoundEffectManager : MonoBehaviour {
+	private static SoundEffectManager Instance;
+	private SoundEffectLibrary soundEffectLibrary;
+	private AudioSource globalSource;
+
+	[Range(0, 1)]
+	public float globalVolume = 0.5f;
+
+	private void Awake() {
+		if (Instance == null) {
+			Instance = this;
+			soundEffectLibrary = GetComponent<SoundEffectLibrary>();
+
+			globalSource = gameObject.AddComponent<AudioSource>();
+			globalSource.playOnAwake = false;
+
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	public static void Play(string soundName) {
+		if (Instance == null || Instance.soundEffectLibrary == null) return;
+
+		AudioClip clip = Instance.soundEffectLibrary.GetRandomClip(soundName);
+		if (clip != null) {
+			Instance.globalSource.PlayOneShot(clip, Instance.globalVolume);
+		}
+	}
+
+	public static void Play(string soundName, Vector3 position) {
+		if (Instance == null || Instance.soundEffectLibrary == null) return;
+
+		AudioClip clip = Instance.soundEffectLibrary.GetRandomClip(soundName);
+		if (clip != null) {
+			AudioSource.PlayClipAtPoint(clip, position, Instance.globalVolume);
+		}
+	}
+}
